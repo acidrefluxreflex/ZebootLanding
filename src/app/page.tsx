8d40845b5be3862c45269f958b08e731c7212abe
kabuki <tanuki5sanuki3@icloud.com>
2023-10-19 15:54:55 +0900

    json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,41 @@ import OverviewSection from "@/components/sections/OverviewSection";
 import GetStartSection from "@/components/sections/GetStartSection";
 import BlogSection from "@/components/sections/BlogSection";
 import ContactSection from "@/components/sections/ContactSection";
+import { Product, SoftwareApplication, WithContext } from "schema-dts";
+
+function addProductJsonLd() {
+  return {
+    __html: `{
+      "@context": "http://schema.org",
+      "@type": "SoftwareApplication",
+      "name": "Zeboot:Zen Sexual Content Blocker",
+      "image": "https://zeboot.net/_next/image?url=%2Fimages%2Flanding%2FappIcon.webp&w=96&q=75",
+      "url": "https://zeboot.net/",
+      "applicationCategory": "HealthApplication",
+      "downloadUrl": "https://apps.apple.com/us/app/zeboot/id6447095547?itsct=apps_box_link&itscg=30200",
+      "operatingSystem": "iOS",
+    }
+`,
+  };
+}
+
+const jsonLd: WithContext<SoftwareApplication> = {
+  "@context": "https://schema.org",
+  "@type": "SoftwareApplication",
+  name: "Zeboot:Zen Sexual Content Blocker",
+  image:
+    "https://zeboot.net/_next/image?url=%2Fimages%2Flanding%2FappIcon.webp&w=96&q=75",
+  url: "https://zeboot.net/",
+  applicationCategory: "HealthApplication",
+  downloadUrl:
+    "https://apps.apple.com/us/app/zeboot/id6447095547?itsct=apps_box_link&itscg=30200",
+  operatingSystem: "iOS",
+
+offers: {
+  "@type": "Offer",
+  "price": "0"
+}
+};
 
 export const metadata: Metadata = {
   title: "Zeboot:Zen Sexual Content Blocker",
@@ -33,24 +68,13 @@ export const metadata: Metadata = {
   },
 };
 
-function addProductJsonLd() {
-  return {
-    __html: `{
-      "@context": "http://schema.org",
-      "@type": "SoftwareApplication",
-      "name": "Zeboot:Zen Sexual Content Blocker",
-      "image": "https://zeboot.net/_next/image?url=%2Fimages%2Flanding%2FappIcon.webp&w=96&q=75",
-      "url": "https://zeboot.net/",
-      "applicationCategory": "HealthApplication",
-      "downloadUrl": "https://apps.apple.com/us/app/zeboot/id6447095547?itsct=apps_box_link&itscg=30200",
-      "operatingSystem": "iOS",
-    }
-`,
-  };
-}
-
 export default function Home() {
   return (
+    <>
+    <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
     <main className="flex min-h-screen flex-col items-center justify-between bg-white ">
       <HeroSection />
       <div className="lg:max-w-7xl md:max-w-3xl mx-auto px-4 sm:px-6 md:px-8">
@@ -63,5 +87,6 @@ export default function Home() {
         <ContactSection />
       </div>
     </main>
+    </>
   );
 }
